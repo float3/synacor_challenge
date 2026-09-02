e8656e2cebe7ed9f1ac6ec0c192239bc79e0e47e
hill <hill@hilll.dev>
2026-09-03 01:12:27 +0200

    Add license, Nix flake and Dependabot config

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "synacor_challenge";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = ["rust-src" "rustfmt" "clippy"];
+      };
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        packages = [
+          rustToolchain
+        ];
+      };
+    });
+}
